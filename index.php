b72eb01bbf28af676972223bbe53a4548d84f0ed
milanegri <milan.egri20@gmail.com>
2024-05-21 13:54:43 +0200

    searc bar at index.php to filter by boardgame name

--- index.php
+++ index.php
@@ -8,8 +8,6 @@ $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
 $select_admin = mysqli_query($conn, "SELECT admin FROM `user_form` WHERE id = '$user_id'");
 $row = mysqli_fetch_assoc($select_admin);
 $admin_status = isset($row['admin']) ? $row['admin'] : 0;
-
-
 ?>
 
 <!DOCTYPE html>
@@ -26,7 +24,10 @@ $admin_status = isset($row['admin']) ? $row['admin'] : 0;
 
 <body>
 <?php include 'navbar.php'; ?>
-
+<div class="search-container">
+    <input type="text" class="searchInput" id="searchBar" placeholder="Társasjáték keresése"
+           onkeyup="filterBoardGames()">
+</div>
 <div class="container boardgame-contanier">
     <?php
     if ($admin_status == 1) {
@@ -34,30 +35,49 @@ $admin_status = isset($row['admin']) ? $row['admin'] : 0;
     }
     ?>
     <h2>Társas Játékok</h2>
-    <?php
-    $result = mysqli_query($conn, "SELECT id, name, image FROM boardgame");
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<div class='boardgame'>";
-            if ($row['image'] == "") {
-                echo '<img src=images/nobg.png class="boardgame-image">';
-            } else {
-                echo "<img src='uploaded_img/boardgames/{$row['image']}' alt='{$row['name']}' class='boardgame-image'>";
-            }
-            echo "<a href='boardgame.php?id={$row['id']}'>{$row['name']}</a>";
-            if ($admin_status == 1) {
-                echo "<div class='boardgame-btns'>";
-                echo "<a class=delete-btn-small href='delete.php?id={$row['id']}'> Törlés </a>";
-                echo "<a class='delete-btn-small blue' href='edit_boardgame.php?id={$row['id']}'> Szerkeztés </a>";
+    <div id="boardgameList">
+        <?php
+        $result = mysqli_query($conn, "SELECT id, name, image FROM boardgame");
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<div class='boardgame'>";
+                if ($row['image'] == "") {
+                    echo '<img src=images/nobg.png class="boardgame-image">';
+                } else {
+                    echo "<img src='uploaded_img/boardgames/{$row['image']}' alt='{$row['name']}' class='boardgame-image'>";
+                }
+                echo "<a href='boardgame.php?id={$row['id']}'>{$row['name']}</a>";
+                if ($admin_status == 1) {
+                    echo "<div class='boardgame-btns'>";
+                    echo "<a class=delete-btn-small href='delete.php?id={$row['id']}'> Törlés </a>";
+                    echo "<a class='delete-btn-small blue' href='edit_boardgame.php?id={$row['id']}'> Szerkeztés </a>";
+                    echo "</div>";
+                }
                 echo "</div>";
             }
-            echo "</div>";
+        } else {
+            echo "Nincsenek társasjátékok.";
         }
-    } else {
-        echo "Nincsenek társasjátékok.";
-    }
-    ?>
-</div>
+        ?>
+        <div>
+        </div>
+
+        <script>
+            function filterBoardGames() {
+                const input = document.getElementById('searchBar').value.toLowerCase();
+                const boardgameList = document.getElementById('boardgameList');
+                const boardgames = boardgameList.getElementsByClassName('boardgame');
+
+                Array.from(boardgames).forEach(boardgame => {
+                    const name = boardgame.getElementsByTagName('a')[0].textContent.toLowerCase();
+                    if (name.includes(input)) {
+                        boardgame.style.display = '';
+                    } else {
+                        boardgame.style.display = 'none';
+                    }
+                });
+            }
+        </script>
 
 </body>
 
